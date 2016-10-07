ccef3f1d138d1b82892c5e9a28e6b2df87a0177a
Yannan Chen <u1072146@ad.utah.edu>
2016-10-07 17:05:59 -0600

    shit

---
+++ ColorEduGame/Assets/GameScene.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ChangeToRandomColor()
+    {
+        float r = Random.value;
+        float g = Random.value;
+        float b = Random.value;
+        Color color = new Vector4(r,g,b,1);
+
+        Material mat = GameObject.Find("gameScene/pCube8").GetComponent<MeshRenderer>().material;
+        mat.color = color;
+    }
+}
