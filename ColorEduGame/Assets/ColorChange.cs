9d5e070e05cd54121eb469f700a51ce96e6a2797
John Olav Haraldstad <john.haraldstad@hotmail.com>
2016-10-06 21:44:39 -0600

    Added green combo + tweeks

--- ColorEduGame/Assets/ColorChange.cs
+++ ColorEduGame/Assets/ColorChange.cs
@@ -5,38 +5,86 @@ public class ColorChange : MonoBehaviour {
 	Color blue = new Color (0, 0, 1);
 	Color red = new Color (1, 0, 0);
 	Color yellow = new Color (1, 1, 0);
+	Color green = new Color (0,1,0);
+
 	Color characterColor = new Color (1, 1, 1);
+	int appliedColors = 0;
+
 	void Start()
 	{
-		
+		this.GetComponent<MeshRenderer> ().material.color = characterColor;
 	}
 
 	public void Change2Blue()
 	{
+		if (appliedColors < 2)
+		{
+			if (characterColor == new Color (1, 1, 1))
+			{
+				characterColor = blue;
+			} else
+			{
+				Debug.Log (characterColor);
+				if (characterColor == yellow)
+				{
+					characterColor = green;
+				} else
+				{
+					characterColor = ((characterColor + blue) / 2);
+				}
+			}
 
-		characterColor.r -= 0.05f;
-		characterColor.g -= 0.05f;
-		this.GetComponent<MeshRenderer> ().material.color = characterColor;
+			Debug.Log (characterColor);
+			this.GetComponent<MeshRenderer> ().material.color = characterColor;
+			appliedColors++;
+		}
 	}
 
 	public void Change2Red()
 	{
+		if (appliedColors < 2)
+		{
+			if (characterColor == new Color (1, 1, 1)) 
+			{
+				characterColor = red;
+			}
+			else 
+			{
+				characterColor = ((characterColor + red) / 2);
+			}
 
-		characterColor.g -= 0.05f;
-		characterColor.b -= 0.05f;
-		this.GetComponent<MeshRenderer> ().material.color = characterColor;
+			this.GetComponent<MeshRenderer> ().material.color = characterColor;
+
+			appliedColors++;
+		}
 	}
 
 	public void Change2Yellow()
-	{
-		characterColor.b -= 0.05f;
-
-		this.GetComponent<MeshRenderer> ().material.color = characterColor;
+    { 
+		if (appliedColors < 2)
+		{
+			if (characterColor == new Color (1, 1, 1))
+			{
+				characterColor = yellow;
+			} else
+			{
+				if (characterColor == blue)
+				{
+					characterColor = green;
+				} else
+				{
+					characterColor = ((characterColor + blue) / 2);
+				}
+			}
+			this.GetComponent<MeshRenderer> ().material.color = characterColor;
+			appliedColors++;
+		}
 	}
 
 	public void ResetCharacterColor()
 	{
 		characterColor = new Color (1, 1, 1);
 		this.GetComponent<MeshRenderer> ().material.color = characterColor;
+		appliedColors = 0;
 	}
 }
